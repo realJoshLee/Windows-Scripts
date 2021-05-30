abc550e1ff7b638caa999ffc725a74a11182ddac
Josh Lee <38728523+realJoshLee@users.noreply.github.com>
2021-05-30 19:27:34 -0400

    Update domain-join.ps1

--- domain-join.ps1
+++ domain-join.ps1
@@ -1,3 +1,8 @@
+# Update the $domain variable to your local domain controller address
+# Update the $password variable to the account password
+# Update the username text in the $username variable with the username used to enroll computers in a domain
+#   This account should have admin access
+
 $domain = "domain.local"
 $password = "password" | ConvertTo-SecureString -asPlainText -Force
 $username = "$domain\username" 
