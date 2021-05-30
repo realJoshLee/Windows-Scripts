9e5494e77f6d927e61d8cb83ffb0228ca589bb4f
Josh Lee <38728523+realJoshLee@users.noreply.github.com>
2021-05-30 17:08:26 -0400

    Added Domain Join script.

---
+++ DomainJoin.ps1
@@ -0,0 +1,7 @@
+﻿set-executionpolicy remotesigned
+
+$domain = "domain.local"
+$password = "password" | ConvertTo-SecureString -asPlainText -Force
+$username = "$domain\username" 
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+Add-Computer -DomainName $domain -Credential $credential -OUPath "OU=Staff Computers,DC=domain,DC=local"
