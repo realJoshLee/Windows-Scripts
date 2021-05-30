41a67d527380effe3df35e5cc126fa8adcc69016
Josh Lee <38728523+realJoshLee@users.noreply.github.com>
2021-05-30 19:23:53 -0400

    Update computer-name-change.ps1

--- computer-name-change.ps1
+++ computer-name-change.ps1
@@ -1,3 +1,6 @@
+# Will ask for the new computer name and then you will enter it.
+# This is useful for mass domain deployments
+
 $compname=Read-Host "Computer Name:"
 $PC = Get-WmiObject -Class Win32_ComputerSystem
 $PC.Rename($compname)
